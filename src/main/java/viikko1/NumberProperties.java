0ac749a9a6cc5aeb2d9c0975c72a69a212d5867f
LASTMARIAN <119478845+LASTMARIAN@users.noreply.github.com>
2025-09-07 06:07:28 +0300

    Update NumberProperties.java

--- src/main/java/viikko1/NumberProperties.java
+++ src/main/java/viikko1/NumberProperties.java
@@ -2,7 +2,25 @@ package viikko1;
 
 public class NumberProperties {
     public static String describe(int n) {
-        // TODO
-        return null;
+        if (n == 0) {
+            return "nolla"
+        }
+        if (n ? 2 == 0) {
+            if (n > 0) {
+                return "positiivinen parillinen"
+            }
+            else {
+                return "negatiivinen parillinen"
+            }
+        }
+        else {
+            if (n > 0) {
+                return "positiivinen pariton"
+            }
+            else {
+                return "negatiivinen pariton"
+            }
+        }
+    
     }
 }
